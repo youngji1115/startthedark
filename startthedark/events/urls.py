ec7996cbb88943d481396dc69412aee3f73a2d28
software-experiment4 <se4@gmail.com>
2015-11-16 13:46:43 +0900

    chapter 5

--- startthedark/events/urls.py
+++ startthedark/events/urls.py
@@ -3,6 +3,7 @@ from events import views
 
 
 urlpatterns = [
+	url(r'^create/$', views.create, name='ev_create'),
 	url(r'^tonight/$',views.tonight, name='ev_tonight'),
 ]
 
--- startthedark/events/views.py
+++ startthedark/events/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from events.models import Event
+from events.forms import EventForm
+from dateutil.parser import parse
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -13,4 +18,30 @@ def tonight(request):
 		'events/tonight.html',
 		context,
 		context_instance = RequestContext(request),
+	)
+
+def create(request):
+	form = EventForm(request.POST or None)
+	if form.is_valid():
+		event = form.save(commit=False)
+		event.creator = request.user
+		guessed_date = None
+		for word in event.description.split():
+			try:
+				guessed_date = parse(word)
+				break
+			except ValueError:
+				continue
+		event.start_date = guessed_date
+		event.save()
+		messages.success(request, "Yout event was posted.")
+		if 'next' in request.POST:
+			next = request.POST['next']
+		else:
+			next = reverse('ev_tonight')
+		return HttpResponseRedirect(next)
+	return render_to_response(
+		'events/create.html',
+		{'form':form},
+		context_instance = RequestContext(request)
 	)
