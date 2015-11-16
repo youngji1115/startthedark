6b3ceccab05374132b4a127f807d34ff92efb6e2
software-experiment4 <se4@gmail.com>
2015-11-16 16:53:27 +0900

    Chapter 6

--- startthedark/events/urls.py
+++ startthedark/events/urls.py
@@ -5,6 +5,7 @@ from events import views
 urlpatterns = [
 	url(r'^create/$', views.create, name='ev_create'),
 	url(r'^tonight/$',views.tonight, name='ev_tonight'),
+	url(r'^toggle-attendance/$', views.toggle_attendance, name='ev_toggle_attendance'),
 ]
 
 
--- startthedark/events/views.py
+++ startthedark/events/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+from events.models import Event, Attendance
 from django.template import RequestContext
-from events.models import Event
 from events.forms import EventForm
 from dateutil.parser import parse
 from django.core.urlresolvers import reverse
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def tonight(request):
 	events = Event.objects.today().filter(latest=True)
+	attending = []
+	for event in events:
+		try:
+			Attendance.objects.get(event=event,user=request.user)
+			attending.append(True)
+		except Attendance.DoesNotExist:
+			attending.append(False)
+
 	context = {
-		'events' : events,
+		'events' : zip(events, attending),
 	}
 	return render_to_response(
 		'events/tonight.html',
@@ -40,8 +49,31 @@ def create(request):
 		else:
 			next = reverse('ev_tonight')
 		return HttpResponseRedirect(next)
+	
 	return render_to_response(
 		'events/create.html',
 		{'form':form},
 		context_instance = RequestContext(request)
 	)
+	
+create = login_required(create)
+
+def toggle_attendance(request):
+	try:
+		event_id = int(request.POST['event_id'])
+	except (KeyError, ValueError):
+		raise Http404
+
+	event=get_object_or_404(Event, id=event_id)
+	attendance,created = Attendance.objects.get_or_create(user=request.user, event=event)
+	if created:
+		messages.success(request, ('You are now attending "%s"' % event))
+	else:
+		attendance.delete()
+		messages.success(request, ('You are no longer attending "%s"'%event))
+	next = request.POST.get('next','')
+	if not next:
+		next = reverse('ev_tonignt')
+	return HttpResponseRedirect(next)
+
+toggle_attendance = login_required(toggle_attendance)
